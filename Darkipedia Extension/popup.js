26c434287cb2ae3e84e7f6597901c7196f29eaa9
Yigit S <55548182+yigitsr@users.noreply.github.com>
2024-01-27 17:48:31 +0300

    Add files via upload

---
+++ Darkipedia Extension/popup.js
@@ -0,0 +1,10 @@
+
+// const _btn = document.getElementById('clickBtn');
+// const _header = document.getElementById('header');
+
+
+    
+// _btn.addEventListener('click', function(){
+
+//     _header.style.color= "red";
+// })
